c26ba4a49b7608f2e4fd103f20ddd582334e9e5c
LuisLeal24 <61062727+LuisLeal24@users.noreply.github.com>
2021-10-12 14:52:28 -0500

    Actividad 9

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="styles.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Khand:wght@600&family=Roboto:wght@300&display=swap"
+        rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Material+Icons"
+        rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Material+Icons+Outlined"
+        rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Material+Icons+Round"
+        rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Material+Icons+Sharp"
+        rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Material+Icons+Two+Tone"
+        rel="stylesheet">
+        <title>Actividad 9</title>
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <!-- Site navigation menu -->
+                <nav>
+                    <ul class="navbar">
+                        <li><a href="index.html">Inicio</a>
+                        <li><a href="musings.html">Animales marinos</a>
+                        <li><a href="town.html">Animales terrestres</a>
+                        <li><a href="links.html">Animales voladores</a>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <main>
+            <!-- Main content -->
+            <h1>¡Esta es una página con mucho estilo!</h1>
+            <h2>Bienvenidos a mi página de animales con estilo.</h2>
+            <p>Aún no tiene imágenes pero si tiene estilo. También tiene ligas, aunque no lleven a ningún lado.</p>
+            <p>Pronto tendrá más contenido, ¡así que tengan paciencia!</p>
+        </main>
+        <footer>Hecha con amor el  <?php echo date("d/m/Y")?> con mucho amor.</footer>
+    </body>
+</html>
